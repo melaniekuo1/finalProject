b5e6f0cc1e65297a2b07dcb337fa29915284a9ba
Melanie Kuo <kuo.melanie@gmail.com>
2020-07-14 14:30:15 -0700

    two

--- finalProject/ViewController.swift
+++ finalProject/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var homeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
